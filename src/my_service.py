dfbd7fdd8c5e91e54e1a7a806205e35bd9d3bc01
valentinbiolley <github-action@actions-template-sync.noreply.github.com>
2026-03-10 13:13:58 +0000

    chore(template): merge template changes :up:
    Signed-off-by: valentinbiolley <github-action@actions-template-sync.noreply.github.com>

--- src/my_service.py
+++ src/my_service.py
@@ -5,68 +5,78 @@ from common_code.service.enums import ServiceStatus
 from common_code.common.enums import FieldDescriptionType, ExecutionUnitTagName, ExecutionUnitTagAcronym
 from common_code.common.models import FieldDescription, ExecutionUnitTag
 from common_code.tasks.models import TaskData
-
 # Imports required by the service's model
-import json
-import cv2
-import numpy as np
+# TODO: 1. ADD REQUIRED IMPORTS (ALSO IN THE REQUIREMENTS.TXT)
 
 settings = get_settings()
 
-api_description = """
-Returns the average shade of an image.
+# TODO: 6. CHANGE THE API DESCRIPTION AND SUMMARY
+api_description = """My service
+bla bla bla...
 """
-api_summary = """
-Returns the average shade of an image.
+api_summary = """My service
+bla bla bla...
 """
 
 class MyService(Service):
+    # TODO: 2. CHANGE THIS DESCRIPTION
     """
-    Average shade service model
+    My service model
     """
 
-    # Any additional fields must be excluded of model by adding a leading underscore for Pydantic to work
+    # Any additional fields must be excluded for Pydantic to work
     _model: object
     _logger: Logger
 
     def __init__(self):
         super().__init__(
-            name="Average Shade",
-            slug="average-shade",
+            # TODO: 3. CHANGE THE SERVICE NAME AND SLUG
+            name="My Service",
+            slug="my-service",
             url=settings.service_url,
             summary=api_summary,
             description=api_description,
             status=ServiceStatus.AVAILABLE,
+            # TODO: 4. CHANGE THE INPUT AND OUTPUT FIELDS, THE TAGS AND THE HAS_AI VARIABLE
             data_in_fields=[
-                FieldDescription(name="image", type=[FieldDescriptionType.IMAGE_PNG, FieldDescriptionType.IMAGE_JPEG]),
+                FieldDescription(
+                    name="image",
+                    type=[
+                        FieldDescriptionType.IMAGE_PNG,
+                        FieldDescriptionType.IMAGE_JPEG,
+                    ],
+                ),
             ],
             data_out_fields=[
-                FieldDescription(name="result", type=[FieldDescriptionType.APPLICATION_JSON]),
+                FieldDescription(
+                    name="result", type=[FieldDescriptionType.APPLICATION_JSON]
+                ),
             ],
             tags=[
                 ExecutionUnitTag(
                     name=ExecutionUnitTagName.IMAGE_PROCESSING,
-                    acronym=ExecutionUnitTagAcronym.IMAGE_PROCESSING
+                    acronym=ExecutionUnitTagAcronym.IMAGE_PROCESSING,
                 ),
             ],
-            docs_url="https://docs.swiss-ai-center.ch/reference/services/average-shade/",
-            has_ai=False
+            has_ai=False,
+            # OPTIONAL: CHANGE THE DOCS URL TO YOUR SERVICE'S DOCS
+            docs_url="https://docs.swiss-ai-center.ch/reference/core-concepts/service/",
         )
         self._logger = get_logger(settings)
 
+    # TODO: 5. CHANGE THE PROCESS METHOD (CORE OF THE SERVICE)
     def process(self, data):
-        # Get raw image data
-        raw = data["image"].data
-        img = cv2.imdecode(np.frombuffer(raw, np.uint8), cv2.IMREAD_COLOR)
-        average_color_row = np.average(img, axis=0)
-        average_color = np.average(average_color_row, axis=0)
+        # NOTE that the data is a dictionary with the keys being the field names set in the data_in_fields
+        # The objects in the data variable are always bytes. It is necessary to convert them to the desired type
+        # before using them.
+        # raw = data["image"].data
+        # input_type = data["image"].type
+        # ... do something with the raw data
+
+        # NOTE that the result must be a dictionary with the keys being the field names set in the data_out_fields
         return {
-            "result": TaskData(
-                data=json.dumps({
-                    "Red": int(average_color[2]),
-                    "Green": int(average_color[1]),
-                    "Blue": int(average_color[0])
-                }).encode("utf-8"),
-                type=FieldDescriptionType.APPLICATION_JSON
-            )
+            "result": TaskData(data=..., type=FieldDescriptionType.APPLICATION_JSON)
         }
+
+
+
